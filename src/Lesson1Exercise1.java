c8e31f53d05d53b4c94c1b686ae2d8355b304e36
Copciop23 <ardeleanucoco2@gmail.com>
2023-09-27 03:51:16 +0100

    Lesson1 Exercise 1 done

---
+++ src/Lesson1Exercise1.java
@@ -0,0 +1,13 @@
+/**
+ * Lesson one Exercise 1
+ * @author Bogdan-Cristian Ardeleanu
+ * @version 1
+ */
+
+public class Lesson1Exercise1 {
+    public static void main (String[] args) {
+        System.out.println("Bits and bytes align," +
+                "\nInnovation takes flight," +
+                "\nTech shapes our future.");
+    }
+}
