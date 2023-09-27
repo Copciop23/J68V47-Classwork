4b6d68075c20c3c1f479e5874ab5904059aad94c
30128792 <ardeleanucoco1@gmail.com>
2023-09-27 14:24:14 +0100

    Lesson 5 exercise 1 again

--- src/Lesson5/L5E1.java
+++ src/Lesson5/L5E1.java
@@ -1,4 +1,17 @@
 package Lesson5;
-
+import java.util.*;
 public class L5E1 {
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);;
+        System.out.println("Enter a number: ");
+        int number = input.nextInt();
+        for( int count=1; count<=12; count++ )
+        {
+            int total = count * number;
+
+
+            System.out.println(number + " x " + count + " = " + total);
+        }
+
+    }
 }
