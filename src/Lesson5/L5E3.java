a7819ba8fe14ac6ba2d4fc70abb60a3cfbb4ba2e
30128792 <ardeleanucoco1@gmail.com>
2023-09-27 14:35:36 +0100

    Lesson 5 exercise 2 and 3 complete

---
+++ src/Lesson5/L5E3.java
@@ -0,0 +1,27 @@
+package Lesson5;
+import java.util.*;
+
+
+public class L5E3 {
+    public static void main (String[] args) {
+        Random rand = new Random();
+        int height = 50;
+        int totalscore = 0;
+
+        Scanner input = new Scanner(System.in);;
+        for( int count=10; count>=1; count-- )
+        {
+            int randomnum1 = rand.nextInt(height);
+            int randomnum2 = rand.nextInt(height);
+
+            int total = randomnum1 + randomnum2;
+            System.out.println("What does " + randomnum1 + " + " + randomnum2 + " equal to?");
+            int numbeeeeeer = input.nextInt();
+            if (numbeeeeeer == total){
+              totalscore = totalscore + 1;
+            };
+        }
+        System.out.println("Your score is: " + totalscore);
+
+    }
+}
