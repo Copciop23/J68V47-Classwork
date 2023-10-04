c4c4d6b06c22b8250e79f938fd13491d8544610a
30128792 <ardeleanucoco1@gmail.com>
2023-10-04 14:41:55 +0100

    Lesson 6 exercise 1, 2 and 3 complete

--- src/Lesson6/L6E2.java
+++ src/Lesson6/L6E2.java
@@ -23,22 +23,22 @@ public class L6E2 {
             if (choice == 2){
                 System.out.println("The time is 14:06 pm");
             }
-            if (choice == 3){
+            if (choice == 3) {
                 int randomnum1 = rand.nextInt(1, numberofchoices);
-                if (randomnum1 == 1){
-                    System.out.println("Did you hear about the cartoonist found dead at his home? Details are sketchy.");
-                }
-                if (randomnum1 == 2){
-                    System.out.println("A sandwich walks into a bar. The barman says: 'sorry we don't serve food here'");
-                }
-                if (randomnum1 == 3){
-                    System.out.println("Where did Napoleon keep his armies? Up his sleevies!");
-                }
-                if (randomnum1 == 4){
-                    System.out.println("Let me free of my misery I do not want to send out dad jokes anymore please LET ME FREE!!!");
-                }
-
-            }
+                switch (randomnum1) {
+                    case 1:
+                        System.out.println("Did you hear about the cartoonist found dead at his home? Details are sketchy.");
+                        break;
+                    case 2:
+                        System.out.println("A sandwich walks into a bar. The barman says: 'sorry we don't serve food here'");
+                        break;
+                    case 3:
+                        System.out.println("Where did Napoleon keep his armies? Up his sleevies!");
+                        break;
+                    case 4:
+                        System.out.println("Let me free of my misery I do not want to send out dad jokes anymore please LET ME FREE!!!");
+                        break;
+                }}
             if (choice == 4){
                 System.out.println("You have finally quit the program, Goodbye after" + count + " tries");
                 start = false;
